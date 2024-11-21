5486166656d3a6b1dc8312310484502825c3a3e3
Biswojit123 <biswojitjena@gmail.com>
2024-11-21 12:24:22 +0530

    Add Data binding form

--- src/main/java/com/biswo/request/SearchData.java
+++ src/main/java/com/biswo/request/SearchData.java
@@ -1,5 +1,14 @@
 package com.biswo.request;
 
-public class SearchData {
+import java.time.LocalDate;
 
+import lombok.Data;
+
+@Data
+public class SearchData {
+	private String planName;
+	private String planStatus;
+	private String gender;
+	private LocalDate plan_start_date;
+	private LocalDate plan_end_date;
 }
