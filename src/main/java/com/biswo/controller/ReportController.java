d279a07184ed747cb4806fe0a15c72e68f8ba13a
Biswojit123 <biswojitjena@gmail.com>
2024-12-05 16:54:10 +0530

    Report Generation And send to email that report in the from of excel/pdf functionality complete successfully

--- src/main/java/com/biswo/controller/ReportController.java
+++ src/main/java/com/biswo/controller/ReportController.java
@@ -6,9 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.biswo.request.SearchData;
 import com.biswo.service.ServiceReport;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class ReportController {
@@ -16,7 +17,21 @@ public class ReportController {
 	@Autowired
 	private ServiceReport service;
 	
-	@PostMapping("/search")
+	@GetMapping("/excel")
+	public void exportToExcel(HttpServletResponse response)throws Exception {
+		response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+		response.setHeader("Content-Disposition", "attachment; filename=users.xlsx");
+		service.exportToExcel(response);
+	}
+	
+	@GetMapping("/pdf")
+	public void exportToPdf(HttpServletResponse response)throws Exception {
+		response.setContentType("application/pdf");
+		response.setHeader("Content-Disposition", "attachment; filename=users.pdf");
+		service.exportToPdf(response);
+	}
+	
+	@PostMapping("/search") 
 	public String handelSearch(@ModelAttribute("search")SearchData requestData,Model model) {
 		System.out.println(requestData);
 		model.addAttribute("plans", service.search(requestData));
--- src/main/java/com/biswo/service/ServiceReport.java
+++ src/main/java/com/biswo/service/ServiceReport.java
@@ -5,10 +5,12 @@ import java.util.List;
 import com.biswo.entity.CityzenPlanInfo;
 import com.biswo.request.SearchData;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 public interface ServiceReport {
 	public List<String> getPlanName();
 	public List<String> getPlanStatus();
 	public List<CityzenPlanInfo> search(SearchData search);
-	public boolean exportToExcel();
-	public boolean exportToPdf();
+	public boolean exportToExcel(HttpServletResponse reponse) throws Exception;
+	public boolean exportToPdf(HttpServletResponse response)throws Exception;
 }
